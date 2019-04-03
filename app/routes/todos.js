65034653607fbab9191c1db9a4b3810a52f9cfa9
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-04-04 00:14:39 +0530

    mirage for todos

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,16 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        let todos = [
-            {
-                title: "Learn Ember",
-                complete: true,
-            },
-            {
-                title: "Solve World Hunger",
-                complete: false,
-            }
-        ];
-        return todos;
+        return this.store.findAll('todo');
     }
 });
