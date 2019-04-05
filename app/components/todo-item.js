beb3879eabef8ca1a6f2ba91c90f27134c70a680
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-04-05 20:29:31 +0530

    edit and delete in component js

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -1,5 +1,29 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-	tagName: 'li'
+	tagName: 'li',
+	classNameBindings: ['editing'],
+	editing: false,
+	actions: {
+		editTodo(){
+			this.toggleProperty('editing');
+
+		},
+		submitTodo() {
+			const todo = this.get("todo");
+			debugger;
+			console.log("submit called");
+			if (todo.get("title") == "") {
+				console.log("destroyRecord");
+				todo.destroyRecord().then(() => {
+					this.toggleProperty("editing");
+				});
+			} else {
+				this.toggleProperty("editing");
+			}
+		},
+		deleteTodo(){
+			this.get("todo").destroyRecord();
+		}
+	}
 });
