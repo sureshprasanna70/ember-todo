b7d0c86a0618086fb83ef911c908fe9cbfadbf9d
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-04-05 20:31:32 +0530

    routes for complete and incomplete

--- app/routes/todos/complete.js
+++ app/routes/todos/complete.js
@@ -1,4 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+	model() {
+		let todos = this.modelFor('todos');
+		return todos.filter((todo) => {
+			return todo.get('complete')
+		})
+	},
+	renderTemplate(controller, model) {
+		this.render('todos.index', {
+			model: model
+		});
+	}
 });
