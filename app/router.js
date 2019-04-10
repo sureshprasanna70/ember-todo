e987ddc7fd31c0f24734d2bc8be871a4b768d39c
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-04-10 11:06:38 +0530

    project route

--- app/router.js
+++ app/router.js
@@ -2,15 +2,20 @@ import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 
 const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
+    location: config.locationType,
+    rootURL: config.rootURL
 });
 
 Router.map(function() {
-    this.route('todos', { path: '/' }, function() {
+  this.route('todos', { path: '/' }, function() {
       this.route('complete');
       this.route('incomplete');
-    });
+  });
+  this.route('projects',function(){
+      this.route('ongoing');
+      this.route('done');
+      this.route('tobe');
+  });
 });
 
 export default Router;
