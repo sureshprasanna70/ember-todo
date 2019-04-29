50e161f5c6c8f6053c648b921f8cba87ce3731e2
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-04-29 17:22:16 +0530

    define validations for project model

--- app/models/project.js
+++ app/models/project.js
@@ -1,6 +1,13 @@
 import DS from 'ember-data';
+import { validator, buildValidations } from 'ember-cp-validations';
 
-export default DS.Model.extend({
+const Validations = buildValidations({
+	title: validator('presence',true),
+	business_analyst: validator('presence',true),
+	status: validator('presence',true)
+});
+
+export default DS.Model.extend(Validations, {
 	title: DS.attr('string'),
 	business_analyst: DS.attr('string'),
 	status: DS.attr('number')
