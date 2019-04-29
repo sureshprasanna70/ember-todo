e72ef8fd5354b1b38a085029da0a84b9751599ea
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-04-29 17:23:59 +0530

    introduce projects/index controller for newProject

---
+++ app/controllers/projects/index.js
@@ -0,0 +1,7 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+	newProject: Ember.computed(function(){
+		return this.get('store').createRecord("project")
+	})
+});
