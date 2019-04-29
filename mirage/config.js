7031d649359e32b064ed6dd0aef10931bb0b509d
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-04-29 17:28:54 +0530

    add post,patch and delete routes

--- mirage/config.js
+++ mirage/config.js
@@ -4,4 +4,7 @@ export default function() {
     this.patch('/todos/:id');
     this.del('/todos/:id');
     this.get('/projects');
+    this.post('/projects');
+    this.patch('/projects/:id');
+    this.del('/projects/:id');
 }
