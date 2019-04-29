b119b78658d7ab9647d52037480e85983ca887ff
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-04-29 17:27:47 +0530

    remove renderController method in routes

--- app/routes/projects/done.js
+++ app/routes/projects/done.js
@@ -4,10 +4,5 @@ export default Route.extend({
 	model() {
 		let projects = this.modelFor('projects');
 		return projects.filterBy('status',3);
-	},
-	renderTemplate(controller, model) {
-		this.render('projects.done', {
-			model: model
-		});
 	}
 });
