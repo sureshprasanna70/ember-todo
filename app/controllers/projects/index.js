851556e4776d2256208028c3b55fd06cb15fdff6
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-05-02 21:51:34 +0530

    projects actions for edit and delete

--- app/controllers/projects/index.js
+++ app/controllers/projects/index.js
@@ -3,5 +3,13 @@ import Controller from '@ember/controller';
 export default Controller.extend({
 	newProject: Ember.computed(function(){
 		return this.get('store').createRecord("project")
-	})
+	}),
+	actions:{
+		editProject(){
+
+		},
+		deleteProject(){
+			
+		}
+	}
 });
