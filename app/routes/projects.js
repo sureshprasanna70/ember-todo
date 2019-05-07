0a9c8b01ed0f6215529e3ba77c4aedbacca1a936
Suresh Prasanna <sureshprasanna70@gmail.com>
2019-05-07 19:51:16 +0530

    use AuthenticatedMixins for projects and todos

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -1,6 +1,7 @@
-import Ember from 'ember';
+import Route from '@ember/routing/route';
+import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend({
+export default Route.extend(AuthenticatedRouteMixin,{
     model() {
         return this.store.findAll('project');
     }
